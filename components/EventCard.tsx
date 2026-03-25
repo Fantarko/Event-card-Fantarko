8fa2a17cabf35b1af7df7260f906e704f13e214e
Fantarko <fantalopper@gmail.com>
2026-03-25 21:09:35 +0700

    implement-posthog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Link from "next/link"
 import Image from "next/image"
+import posthog from "posthog-js"
 
 interface Props{
     title:string;
@@ -12,21 +14,21 @@ interface Props{
 
 const EventCard = ({title,image,slug,location,date,time}:Props) => {
   return (
-    <Link href={`/events/${slug}`} id="event-card">
-        <Image src={image} alt={title} width={410} height={300} className="poster" />
+    <Link href={`/events/${slug}`} id="event-card" onClick={() => posthog.capture('event_card_clicked', { title, slug, location, date })}>
+        <Image src={image} alt={title} width={410} height={300} priority className="poster w-auto h-auto" />
         <div className="flex flex-col gap-2">
-            <Image src="/icons/pin.svg" alt="Location" width={14} height={14} />
+            <Image src="/icons/pin.svg" alt="Location" width={14} height={14} className="w-[14px] h-[14px] object-contain" />
             <p className="location">{location}</p>
         </div>
         <p className="title">{title}</p>
 
         <div className="datetime">
             <div>
-                <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+                <Image src="/icons/calendar.svg" alt="date" width={14} height={14}  className="w-[14px] h-[14px] object-contain"/>
                 <p className="date">{date}</p>
             </div>
             <div>
-                <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+                <Image src="/icons/clock.svg" alt="time" width={14} height={14} className="w-[14px] h-[14px] object-contain" />
                 <p className="time">{time}</p>
             </div>
         </div>
